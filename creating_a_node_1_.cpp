4d6eaaade1c64a403861a6b337decf4bb46266dd
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-06-29 06:30:26 +0600

    Creating node

---
+++ creating_a_node_1_.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+};
+
+
+int main()
+{
+    Node a, b, c;
+
+    a.val = 10;
+    b.val = 20;
+    c.val = 30;
+
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    cout << a.val << endl;
+
+    // cout << (*a.next).val << endl;
+    cout << a.next->val << endl; //Printing the value of b;
+    cout << a.next->next->val << endl; //Printing the value of c;
+
+
+
+    return 0;
+}
